9be9acd085797dabf6276b8d670d1ab3cef43998
Tarik Amehri <amehritarik@gmail.com>
2025-06-29 15:12:58 +0100

    ex00

---
+++ 00/ex00/Hello.py
@@ -0,0 +1,15 @@
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
+
+#your code here
+ft_list[1] = 'World!'
+ft_tuple = ('Hello', 'Morocco!')
+ft_set = {"Hello", "Khouribga!"}
+ft_dict["Hello"] = '42Khouribga!'
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
