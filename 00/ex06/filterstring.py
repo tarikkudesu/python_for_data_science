8ea79385f11450033897358fcd5c94021c844b88
Tarik Amehri <amehritarik@gmail.com>
2025-07-02 09:43:44 +0100

    ex07 osubtle change

--- 00/ex06/filterstring.py
+++ 00/ex06/filterstring.py
@@ -10,19 +10,18 @@ def main():
         that have a length greater than N.
     """
 
-    chars = 'abcdefghijklmnopqrstuvwxyzABCDEF \
-        GHIJKLMNOPQRSTUVWXYZ0123456789'
+    chars = 'abcdefghijklmnopqrstuvwxyz \
+        ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
     ac = len(sys.argv)
     av = sys.argv
 
     try:
-        assert ac == 3, "the arguments are bad"
-        assert av[2].isdigit(), "the arguments are bad"
-        assert len([c for c in av[1] if c in chars]) == len(av[1]), \
-            "the arguments are bad"
+        assert ac == 3
+        assert av[2].isdigit()
+        assert len([c for c in av[1] if c in chars]) == len(av[1])
         print(ft_filter(lambda ele: len(ele) > int(av[2]), av[1].split()))
     except AssertionError as e:
-        print("AssertionError: ", e)
+        print("AssertionError: the arguments are bad")
 
 
 if __name__ == '__main__':
