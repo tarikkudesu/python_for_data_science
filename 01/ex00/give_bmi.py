517e75dda82d3c9169385397db62cbfbcfb1b95a
Tarik Amehri <amehritarik@gmail.com>
2025-07-02 20:09:26 +0100

    ex00

--- 01/ex00/give_bmi.py
+++ 01/ex00/give_bmi.py
@@ -1,22 +1,64 @@
 import numpy as np
 
 
-def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+def give_bmi(height: list[int | float],
+             weight: list[int | float]) -> list[int | float]:
     """
 
-	give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]
-	returns a list of BMI values based on 2 lists of integers or floats that come as input
+    give_bmi(height: list[int | float], weight: \
+        list[int | float]) -> list[int | float]
+    returns a list of BMI values based on 2 lists \
+        of integers or floats that come as input
 
     """
-    assert len(height) == len(weight)
-    return list(np.divide(np.array(weight), np.multiply(np.array(height), np.array(height))))
+    try:
+        return list(np.divide(np.array(weight),
+                              np.multiply(np.array(height), np.array(height))))
+    except (TypeError, ValueError):
+        print("AssertionError: the arguments are bad")
+        return []
 
 
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
     """
 
-	apply_limit(bmi: list[int | float], limit: int) -> list[bool]
-	returns a list of booleans (True if above the limit) based on a list of integers or floats and an integer representing a limit as parameters
+    apply_limit(bmi: list[int | float], limit: int) -> list[bool]
+    returns a list of booleans (True if above the limit) \
+        based on a list of integers or floats and an integer \
+            representing a limit as parameters
 
-	"""
-    return list(np.array(bmi) > limit)
+    """
+    try:
+        assert type(limit) is int
+        return list(np.array(bmi) > limit)
+    except (TypeError, ValueError, AssertionError):
+        print("AssertionError: the arguments are bad")
+        return []
+
+
+# tests
+# from give_bmi import give_bmi, apply_limit
+# height = [2.71, 1.15]
+# weight = [165.3, 38.4]
+# bmi = give_bmi(height, weight)
+# print(bmi, type(bmi))
+# print(apply_limit(bmi, 26))
+# print()
+# height = [2.71, 1.15, "d"]
+# weight = [165.3, 38.4]
+# bmi = give_bmi(height, weight)
+# print(bmi, type(bmi))
+# print(apply_limit(bmi, 26))
+# print()
+# height = [2.71, 1.15, 2]
+# weight = [165.3, 38.4]
+# bmi = give_bmi(height, weight)
+# print(bmi, type(bmi))
+# print(apply_limit(bmi, 26))
+# print()
+# height = [2.71, 1.15, 2]
+# weight = [165.3, 38.4]
+# bmi = give_bmi(height, weight)
+# print(bmi, type(bmi))
+# print(apply_limit(bmi, "4"))
+# print()
